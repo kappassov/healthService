2ac7c8dd8c9947476d478b3e5ca5658a23b9a606
kappassov <abayk0708@gmail.com>
2021-12-01 22:07:58 +0600

    added ssl configuration to db connection

--- db/index.js
+++ db/index.js
@@ -8,6 +8,9 @@ const proConfig = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
