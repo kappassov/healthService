8f845af7fcbe293c654bd234df1cdd5f029be9e1
kappassov <abayk0708@gmail.com>
2021-12-06 15:57:01 +0600

    removed path

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ app.use(morgan("dev"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 }
 
 //CREATE PUB SERVANT
