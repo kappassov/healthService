b498233dc31128404e1c354ca70dfffc51caffe0
kappassov <abayk0708@gmail.com>
2021-12-06 18:02:39 +0600

    changed

--- server.js
+++ server.js
@@ -335,6 +335,13 @@ app.delete("/api/record/:id", async (req, res) => {
   }
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server has started on PORT ${PORT}`);
 });
